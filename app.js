760eeaf9b8163a34d4a5a63ee54bcee5938e8ffc
devWizardFk <farzadk10000@gmail.com>
2023-12-22 10:20:12 +0530

    ready to play

---
+++ app.js
@@ -0,0 +1,67 @@
+const btnColors = ["red", "blue", "green", "yellow"]
+let gamePattern = []
+let userCLickedPattern = []
+let started = false;
+let level = 0;
+
+function nextSequence() {
+    userCLickedPattern = []
+    let randomNum = Math.floor(Math.random()*4)
+    let randomColor = btnColors[randomNum]
+    gamePattern.push(randomColor)
+    $('#' + randomColor).fadeIn().fadeOut().fadeIn()
+    playSound(randomColor)
+    level++;
+    $('h1').text(`Level ${level}`)
+}
+
+$('.'+'btn').click(function clickHandler() {
+    if(started) {
+        let userChosenColor = this.classList[1]
+        playSound(userChosenColor)
+        animatePress(userChosenColor)
+        userCLickedPattern.push(userChosenColor)
+        checkAnswer(userCLickedPattern.length-1)
+    }
+})
+
+function playSound(name) {
+    let audio = new Audio(`./sounds/${name}.mp3`)
+    audio.play()
+}
+
+function animatePress(currColor) {
+    $(`#${currColor}`).addClass('pressed')
+    setTimeout(function() {
+        $(`#${currColor}`).removeClass('pressed')
+    }, 100)
+}
+
+$(document).keydown(function() {
+    if(!started) {
+        started = true;
+        nextSequence();
+    }
+})
+
+function checkAnswer(currLevel) {
+    if(userCLickedPattern[currLevel] === gamePattern[currLevel]) {
+        if(userCLickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence()
+            }, 1000)
+        }
+    }
+    else {
+        $('body').addClass('game-over')
+        setTimeout(function() {
+            $('body').removeClass('game-over')
+        }, 200)
+        $('h1').text('Game Over! Press any key to restart')
+        playSound('wrong')
+        level = 0
+        started = false;
+        gamePattern = []
+    }
+}
+
